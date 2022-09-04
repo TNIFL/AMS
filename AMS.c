3937895b2c3ff79d9d5fcc87bd3772f36c414e4e
TNIFL <tnifl@naver.com>
2022-09-05 00:12:11 +0900

    AMS

--- AMS.c
+++ AMS.c
@@ -1,6 +1,6 @@
 //주소관리 시스템
 //주소(address), 소유주 이름(owner name), 사람 인원(number of people), 건물 이름(building name), 건축 날짜(date of construction)
-// 1 : 생성. 2 : 조회. 3 : 수정. 4 : 삭제
+// 1 : 생성 2 : 조회 3 : 수정 4 : 삭제
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
@@ -34,12 +34,33 @@ void InsertData(struct Node *target, AMS ams) {
 
 AMS * RetrieveData(struct Node *head, int key) {
     struct Node *current = head;
-    while (current->next != NULL) {
+    while (current != NULL) {
         if (current->ams.key == key) {
             return &current->ams;
+        } else {
+            current = current->next;
         }
     }
+    return NULL;
 }
+
+
+//void ModifyData(struct Node *head, int key, AMS ams) {
+//    struct Node *current = head;
+//    struct Node *overwrite = malloc(sizeof(struct Node));
+//    while (current != NULL) {
+//        if (current->ams.key == key) {
+//            overwrite->ams.key = ams.key;
+//            overwrite->ams.NumberOfPeople = ams.NumberOfPeople;
+//            overwrite->ams.DateOfConstruction = ams.DateOfConstruction;
+//            strcpy(overwrite->ams.address, ams.address);
+//            strcpy(overwrite->ams.OwnerName, ams.OwnerName);
+//            strcpy(overwrite->ams.BuildingName, ams.BuildingName);
+//            overwrite->next = head->next;
+//            head->next = overwrite;
+//        }
+//    }
+//}
 
 void DeleteData(struct Node *head, int key) {
     struct Node *temp = NULL;
@@ -57,19 +78,45 @@ void DeleteData(struct Node *head, int key) {
     
 }
 
-//int create() {
-//
-//    printf("주소 입력 : ");
-//    scanf("%s")
-//
-//
-//    return  ;
+//void ModifyData(struct Node *head, int key, AMS ams) {
+//    DeleteData(head, 2);
+//    InsertData(head, ams);
 //}
 
+void ModifyData(struct Node *head, int key, AMS ams) {
+//    struct Node * current = head;
+//    struct Node * overwrite = malloc(sizeof(struct Node));
+//    while (current != NULL) {
+//        if (current->ams.key == key) {
+//            overwrite->ams.key = ams.key;
+//            overwrite->ams.NumberOfPeople = ams.NumberOfPeople;
+//            overwrite->ams.DateOfConstruction = ams.DateOfConstruction;
+//            strcpy(overwrite->ams.address, ams.address);
+//            strcpy(overwrite->ams.OwnerName, ams.OwnerName);
+//            strcpy(overwrite->ams.BuildingName, ams.BuildingName);
+//            break;
+//        }
+//        else {
+//            current = current->next;
+//        }
+//    }
+    struct Node * current = head;
+    while (current != NULL) {
+        if (current->ams.key == key) {
+            current->ams.NumberOfPeople = ams.NumberOfPeople;
+            current->ams.DateOfConstruction = ams.DateOfConstruction;
+            strcpy(current->ams.address, ams.address);
+            strcpy(current->ams.OwnerName, ams.OwnerName);
+            strcpy(current->ams.BuildingName, ams.BuildingName);
+            return;
+        } else {
+            current = current->next;
+        }
+    }
+}
 
 
 int main(void) {
-    int navigation;
     struct Node *head = malloc(sizeof(struct Node));
     
     
@@ -79,6 +126,7 @@ int main(void) {
     
     AMS * testData1 = malloc(sizeof(AMS));
     AMS * testData2 = malloc(sizeof(AMS));
+    AMS * testData3 = malloc(sizeof(AMS));
     
     AMS * retrievedData = NULL;
     
@@ -90,15 +138,34 @@ int main(void) {
     strcpy(testData2->address, "와우");
     strcpy(testData2->OwnerName, "유동관");
     
+    testData3->key = 3;
+    strcpy(testData3->address, "홀리쉣");
+    strcpy(testData3->OwnerName, "관동유");
+    
 
     InsertData(head, *testData1);
     InsertData(head->next, *testData2);
     
     printf("%s", head->next->ams.address);
     printf("%s", head->next->ams.OwnerName);
+    printf("\n");
     
-    retrivedData = RetrieveData(head, 1);
+    retrievedData = RetrieveData(head, 2);
     
-    printf("%s", retrivedData->address);
-    printf("%s", retrivedData->OwnerName);
+    printf("%s", retrievedData->address);
+    printf("%s", retrievedData->OwnerName);
+    printf("\n");
+    
+    
+    
+    ModifyData(head, 2, *testData3);
+    ModifyData(head, 2, *testData3);
+    
+    testData3 = RetrieveData(head, 2);
+    
+    printf("%s", testData3->address);
+    printf("%s", testData3->OwnerName);
+    printf("\n");
 }
+
+
